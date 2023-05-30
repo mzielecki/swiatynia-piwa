2d67a70c5f373533933e6cc88cc2d78d67398c6e
Shadoweqq <mateusz.zielecki@gmail.com>
2023-05-30 23:16:16 +0200

    fixes and added box under every type

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import Header from "./components/header/header";
-import Carousel from "./components/carousel/carousel";
-import Footer from "./components/footer/footer";
+import Header from './components/header/header';
+import Carousel from './components/carousel/carousel';
+import Footer from './components/footer/footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <h2 className="heading-text">Świątynia piwa</h2>
-      <Carousel></Carousel>
-      <Footer></Footer>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header></Header>
+			<h2 className="heading-text">ŚWIĄTYNIA PIWA</h2>
+			<Carousel></Carousel>
+			<Footer></Footer>
+		</div>
+	);
 }
 
 export default App;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 import './header.css';
 
 const Header = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 
-  const handleMenuToggle = () => {
-    setMenuOpen(!menuOpen);
-  };
+	const handleMenuToggle = () => {
+		setMenuOpen(!menuOpen);
+	};
 
-  return (
-    <header className="header">
-      <div className="logo"></div>
-      <div className={`menu ${menuOpen ? 'open' : ''}`}>
-        <div className='item'>Nasze piwa</div>
-        <div className='item'>O nas</div>
-        <div className='item'>Kontakt</div>
-      </div>
-      <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={handleMenuToggle}>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </header>
-  );
+	return (
+		<header className="header">
+			<div className="logo"></div>
+			<div className={`menu ${menuOpen ? 'open' : ''}`}>
+				<div className="item">Nasze piwa</div>
+				<div className="item">O nas</div>
+				<div className="item">Kontakt</div>
+			</div>
+			<div
+				className={`menu-toggle ${menuOpen ? 'open' : ''}`}
+				onClick={handleMenuToggle}
+			>
+				<div className="bar"></div>
+				<div className="bar"></div>
+				<div className="bar"></div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
